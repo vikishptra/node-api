88d60385317bc9642f18332b2738494b4733c82f
shnooki <vikisahputra17@gmail.com>
2023-05-30 17:02:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 8001;
+
+// Konfigurasi koneksi MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cloud-api'
+});
+
+// Menghubungkan ke MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error('Koneksi ke MySQL gagal: ', err);
+    return;
+  }
+  console.log('Terhubung ke MySQL');
+});
+
+// Mengatur endpoint untuk mendapatkan data
+app.get('/', (req, res) => {
+  // Query untuk mendapatkan data dari tabel
+  const query = 'SELECT * FROM buku';
+
+  // Eksekusi query
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error saat mengambil data: ', err);
+      res.status(500).send('Terjadi kesalahan');
+      return;
+    }
+    res.json(results);
+  });
+});
+
+// Menjalankan server
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
